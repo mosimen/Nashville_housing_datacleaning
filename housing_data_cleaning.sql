60970aea0e249e47676cad20072ccf1e515e1478
mosimen <97052635+mosimen@users.noreply.github.com>
2022-04-18 09:08:40 +0100

    Update housing_data_cleaning.sql

--- housing_data_cleaning.sql
+++ housing_data_cleaning.sql
@@ -9,7 +9,7 @@ select
 from NashvilleHousing
 --We see that the SaleDate data is in datetime format, we want to convert it to date format
 
---Add a new column with the right data type to the housing table, then set it to the SaleDate data
+--Add a new column with the right datatype to the housing table, then set it to the SaleDate data
 alter table [dbo].[NashvilleHousing]
 add sales_date date;
 
@@ -20,18 +20,22 @@ set sales_date = cast(SaleDate as date)
 select SaleDate, sales_date
 from NashvilleHousing
 
----Populate Property address data
+---Split the PropertyAddress data into Address, City and State
 select PropertyAddress
 from NashvilleHousing
+
+--Check for Nulls in the data
+select PropertyAddress
+from NashvilleHousing
+where PropertyAddress is null
 --We see null values in the PropertyAddress column
 
 select *
 from NashvilleHousing
---where PropertyAddress is null
 order by ParcelID
-----But we notice that ParcelID that is that same has same PropertyAddress 
+---But we notice that the same ParcelID has same PropertyAddress, we need to fill the null values
 
---We will join the dataset with itself
+--We will join the dataset with itself inorder to fill the null values
 select a.ParcelID, a.PropertyAddress, b.ParcelID, b.PropertyAddress, ISNULL(a.PropertyAddress,b.PropertyAddress)
 from NashvilleHousing as a
 join NashvilleHousing as b
@@ -67,13 +71,11 @@ from NashvilleHousing
 
 alter table NashvilleHousing
 add PropertySplitAddress nvarchar(255);
-
 update NashvilleHousing
 set PropertySplitAddress = SUBSTRING(PropertyAddress, 1, CHARINDEX(',', PropertyAddress)-1)
 
 alter table NashvilleHousing
 add PropertySplitCity nvarchar(255)
-
 update NashvilleHousing
 set PropertySplitCity = SUBSTRING(PropertyAddress, CHARINDEX(',', PropertyAddress)+1, LEN(PropertyAddress))
 
@@ -81,12 +83,12 @@ select *
 from NashvilleHousing
 
 
---Extracting Address, City and State but we will not use substring this time. Let us try the PARSENAME method
+--Extracting Address, City and State from OwnerAddress column but we will not use substring this time. Let us try the PARSENAME method
 select *
 from NashvilleHousing
 -- OwnerAddress is null
 
----
+---PARSENAME method
 select
 PARSENAME(replace(OwnerAddress, ',', '.'),3) as owner_address,
 PARSENAME(replace(OwnerAddress, ',', '.'),2) as owner_city,
@@ -97,19 +99,16 @@ where OwnerAddress is not null
 
 alter table [dbo].[NashvilleHousing]
 add owner_address nvarchar(255)
-
 update NashvilleHousing
 set owner_address = PARSENAME(replace(OwnerAddress, ',', '.'),3)
 
 alter table [dbo].[NashvilleHousing]
 add owner_city nvarchar(255)
-
 update NashvilleHousing
 set owner_city = PARSENAME(replace(OwnerAddress, ',', '.'),2)
 
 alter table [dbo].[NashvilleHousing]
 add owner_state nvarchar(255)
-
 update NashvilleHousing
 set owner_state = PARSENAME(replace(OwnerAddress, ',', '.'),1)
 
@@ -134,7 +133,7 @@ set SoldAsVacant = case
 	else SoldAsVacant
 	end
 
----Removing Duplicates
+---Removing Duplicate data
 ; with cte as
 (
 select *,
@@ -161,5 +160,8 @@ from NashvilleHousing
 
 alter table NashvilleHousing
 drop column SaleDate,PropertyAddress,OwnerAddress, TaxDistrict
+
+
+
 
 
